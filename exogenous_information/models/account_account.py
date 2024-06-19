1ea5dcbefbc21bc0bf89e2457985c6f4d50f0052
Cristian Iván Izaquita Morales <cristian.izaquita@gmail.com>
2024-06-19 13:05:10 -0500

    Delete exogenous_information directory

--- exogenous_information/models/account_account.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models, _
-
-class AccountAccount(models.Model):
-    _inherit = 'account.account'
-
-    concepto = fields.Integer(string='Concepto', help='Es el número de concepto de la cuenta ante la DIAN.')
